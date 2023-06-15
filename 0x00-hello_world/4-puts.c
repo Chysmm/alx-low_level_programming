b5c66fe0f917dc60897df530041b5e8eea09a8b8
Chysmm <som4nick@yahoo.com>
2023-06-15 08:22:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  *my_main - main entry point
  *return : always 0 (success)
  */
-int main() {
+int main() 
+{
     // This program returns 0 to indicate successful execution
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
