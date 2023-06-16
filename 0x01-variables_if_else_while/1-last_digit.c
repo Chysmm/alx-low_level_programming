373c2458bb97cc8995cf7f48fe56fc89ccbcfe1a
Chysmm <som4nick@yahoo.com>
2023-06-16 14:47:50 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("Last digit of %d is %d and isgreater than 5\n", n, ld);
+printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 }
 else if (ld == 0)
 {
@@ -25,7 +25,7 @@ printf("Last digit of %d is %d and is 0\n", n, ld);
 }
 else
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 }
 return (0);
 }
