473dfca812ee0d20d402de27f80ac9a9c0ea89ab
Chysmm <som4nick@yahoo.com>
2023-06-20 23:52:17 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,16 +10,21 @@ int main(void)
 {
 int count;
 unsigned long fib1 = 1, fib2 = 2, sum;
+printf("%lu, %lu, ", fib1, fib2);
 for (count = 3; count <= 50; count++)
 {
 sum = fib1 + fib2;
 printf("%lu", sum);
+if (count < 50)
+{
+printf(",");
+}
+else
+{
+printf("\n");
+}
 fib1 = fib2;
 fib2 = sum;
-if (count == 49)
-printf("\n");
-else
-printf(",");
 }
 return (0);
 }
