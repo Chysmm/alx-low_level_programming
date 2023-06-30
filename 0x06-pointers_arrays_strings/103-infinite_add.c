4277e53112de5a5ecd8820ede5db9e431b7bad39
Chysmm <som4nick@yahoo.com>
2023-06-29 23:10:37 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -32,7 +32,7 @@ temp = *(n + j);
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int overflow = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, temp_tot = 0
+int val1 = 0, val2 = 0, temp_tot = 0;
 while (*(n1 + i) != '\0')
 i++;
 while (*(n2 + j) != '\0')
@@ -46,13 +46,13 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - '0';
+val1 = *(n1 + i) -'0';
 if (j < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + j) -'0';
 temp_tot = val1 + val2 + overflow;   if (temp_tot >= 10)
-overflow = 
+overflow = 1;
 else
 overflow = 0;
 if (digits >= (size_r - 1))
